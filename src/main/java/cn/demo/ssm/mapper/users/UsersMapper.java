a35feb35210432fd0ea0d4e1e5d3893bf586186e
night <492407268@qq.com>
2017-12-22 12:00:00 +0800

    修改 UsersMapper

---
+++ src/main/java/cn/demo/ssm/mapper/users/UsersMapper.java
@@ -0,0 +1,36 @@
+package cn.demo.ssm.mapper.users;
+
+import cn.demo.ssm.po.users.Users;
+import cn.demo.ssm.po.users.UsersExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersMapper {
+    int countByExample(UsersExample example);
+
+
+
+    int deleteByExample(UsersExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    List<Users> selectByExample(UsersExample example);
+
+    Users selectByPrimaryKey(Long id);
+
+    Users selectByUserName(String name);
+
+    int updateByExampleSelective(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByExample(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByPrimaryKeySelective(Users record);
+
+    int updateByPrimaryKey(Users record);
+}
